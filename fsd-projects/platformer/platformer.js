046ff7844abda31f0ca6af50fb11f82955e0898d
padmetalbot <padmetalbot@gmail.com>
2026-09-01 15:33:03 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,13 +27,24 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+     toggleGrid();
 
 
     // TODO 2 - Create Platforms
+createPlatform(150, 500, 200, 20, "green");
+createPlatform(330, 700, 50, 50, "red");
+createPlatform(500, 600, 50, 290, "blue");
+createPlatform(500, 400, 200, 20, "yellow");
+createPlatform(800, 350, 50, 50, "green");
+createPlatform(950, 250, 200, 20, "red");
+createPlatform(630, 700, 50, 50, "red");
+createPlatform(1300, 150, 50, 50, "green");
+createCollectable("steve", 1350, 50);
+createCollectable("diamond", 200, 170, 0.5, 0.7);
 
-
-
+createCannon("top", 200, 1500);
+createCannon("top", 400, 2000);
+createCannon("right", 300, 2000);
 
     // TODO 3 - Create Collectables
 
